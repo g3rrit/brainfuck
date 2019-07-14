08cb58abee4a109fb25e902f230175c57a01a85e
pearisgreen <gerrit.proessl@gmail.com>
2019-07-14 14:49:53 +0200

    # Update cleanup hs interpreter

--- brainfuck.hs
+++ brainfuck.hs
@@ -1,29 +1,34 @@
 import System.IO
 import Data.Char
 
-data State = State
-  { mem_in :: [Int]
-  , mem_to :: [Int] }
+data State = State [Int] Int [Int]
 
-val :: State -> Int
-val (State _ (v:m)) = v
+data Action = Rewind | Skip | Set | Print | None
 
-data Action = WRewind | WSkip | Set | Print | None
+--------------------------------------------------------
+-- EVALUATION_EXECUTION
+--------------------------------------------------------
+exec :: State           -- current state
+     -> Char            -- input char
+     -> (State, Action) -- resulting action and state
 
-exec :: State -> Char -> (State, Action)
-exec s c = let State (i:mi) (t:mt) = s in
-             case c of
-               '>' -> ((State (t:(i:mi)) mt), None)
-               '<' -> ((State mi (i:(t:mt))), None)
-               '+' -> ((State (i:mi) ((t + 1):mt)), None)
-               '-' -> ((State (i:mi) ((t - 1):mt)), None)
-               '.' -> (s, Print)
-               ',' -> (s, Set)
-               '[' -> (s, if (val s) == 0 then WSkip else None)
-               ']' -> (s, if (val s) /= 0 then WRewind else None)
-               _ -> (s, None)
+exec s@(State mi@(i:mi') v mt@(t:mt')) c =
+  case c of
+    '>' -> ((State (v:mi) t mt'), None)
+    '<' -> ((State mi' i (v:mt)), None)
+    '+' -> ((State mi (v + 1) mt), None)
+    '-' -> ((State mi (v - 1) mt), None)
+    '.' -> (s, Print)
+    ',' -> (s, Set)
+    '[' -> (s, if v == 0 then Skip else None)
+    ']' -> (s, if v /= 0 then Rewind else None)
+    _ -> (s, None)
 
-next :: State -> [Char] -> [Char] -> IO ()
+next :: State  -- current state
+     -> [Char] -- already read chars
+     -> [Char] -- buffered of yet to read chars
+     -> IO ()  -- IO action
+
 next s to [] = do
   c <- hGetChar stdin
   next s to [c]
@@ -31,11 +36,17 @@ next s to (c:from) = do
   (ns, nt, nf) <- do_action (exec s c) to (c:from)
   next ns nt nf
 
-do_action :: (State, Action) -> [Char] -> [Char] -> IO (State, [Char], [Char])
+--------------------------------------------------------
+-- ACTIONS
+--------------------------------------------------------
+do_action :: (State, Action)            -- current state and action
+          -> [Char]                     -- already read chars
+          -> [Char]                     -- buffered of yet to read chars
+          -> IO (State, [Char], [Char]) -- IO action with next state and tape
 do_action (s, a) to (f:from) =
   case a of
-    WRewind -> let (nt, nf) = do_rewind to (f:from) in return (s, nt, nf)
-    WSkip -> do
+    Rewind -> let (nt, nf) = do_rewind to (f:from) in return (s, nt, nf)
+    Skip -> do
       (nt, nf) <- do_skip (f:to) from
       return (s, nt, nf)
     Set -> do
@@ -46,14 +57,22 @@ do_action (s, a) to (f:from) =
       return (s, (f:to), from)
     None -> return (s, (f:to), from)
 
-do_rewind :: [Char] -> [Char] -> ([Char], [Char])
+-- ------------------ REWIND_ACTION ------------------- --
+do_rewind :: [Char]           -- to buffer
+          -> [Char]           -- from buffer
+          -> ([Char], [Char]) -- resulting to and from buffer
+
 do_rewind (x:xs) l =
   case x of
     '[' -> (xs, (x:l))
     ']' -> let (nxs, nl) = do_rewind xs (x:l) in do_rewind nxs nl
     _ -> do_rewind xs (x:l)
 
-do_skip :: [Char] -> [Char] -> IO ([Char], [Char])
+-- ------------------ SKIP_ACTION ------------------- --
+do_skip :: [Char]              -- to buffer
+        -> [Char]              -- from buffer
+        -> IO ([Char], [Char]) -- resulting to and from buffer as action
+
 do_skip xs [] = do
   c <- hGetChar stdin
   do_skip xs [c]
@@ -66,16 +85,22 @@ do_skip xs (x:l) =
     _ -> do_skip (x:xs) l
 
 
-do_set :: State -> IO State
-do_set (State mi (t:mt)) = do
+-- ------------------ SET_ACTION ------------------- --
+do_set :: State    -- current state
+       -> IO State -- next state as action
+
+do_set (State mi v mt) = do
   c <- hGetChar stdin
-  return (State mi ((ord c):mt))
+  return (State mi (ord c) mt)
 
-do_print :: State -> IO ()
-do_print (State mi (t:mt)) = print $ show t
+-- ------------------ PRINT_ACTION ------------------- --
+do_print :: State -- current state
+         -> IO () -- resulting action
 
+do_print (State mi v mt) = print $ (chr v)
 
+-- ------------------ ENTRY_POINT ------------------- --
 main = do
   putStrLn "Brainfuck hs"
-  next (State mem mem) [] []
+  next (State mem 0 mem) [] []
     where mem = 0 : mem
